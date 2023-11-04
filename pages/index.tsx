cc3db815909f722aa0bdf8ac7b494fa1ae93fd45
deyberst <deyberst@gmail.com>
2023-11-04 17:01:05 -0500

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+const Home = () => {
+  return <main className='h-screen w-full flex items-center justify-center bg-[#722e9a]'>
+    <a href="/login" className="text-lg hover:text-yellow-400 hover:scale-110 text-gray-200 font-bold">Hola mundo bienvenido a mi login</a>
+
+    </main>;
+};
+
+export default Home;
